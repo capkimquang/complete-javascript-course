d38f0d4251685bb54551c8f7faf0001e4121ee5e
capkimquang <kimquang1920@gmail.com>
2020-11-21 16:50:58 +0700

    test

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,31 @@
+/*
+let js = 'amazing';
+if (js === 'amazing') alert('JavaScript is fun');
+console.log(40 + 8 + 23 - 10);
+*/
+
+/*
+// show type of true,
+// typeof is a operator, return a string which is type of the variable passes.
+console.log(typeof true);
+
+// example of dynamic typing
+js = true;
+console.log(typeof js); // return true;
+js = 'hey';
+console.log(typeof js); // return string;
+
+// example of Undefined
+let year;
+console.log(year); // return undefined;
+console.log(typeof year); // return undefined;
+
+year = 1991;
+console.log(typeof year); // return number;
+
+console.log(typeof null); // return object;
+// this is bug, but never corrected.
+*/
+
+let age = 30;
+age = 31; // mutate a variable -> change its value
